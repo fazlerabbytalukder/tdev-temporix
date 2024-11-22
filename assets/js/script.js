60497012dbc4cce581fadccdac6ba244c113599d
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-11-22 15:37:23 +0600

    offcanvas added in toggle button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
+(function ($) {
+  "use strict";
+
+  var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
     navigation: {
-        nextEl: ".custom-btn-next",
-        prevEl: ".custom-btn-prev",
-      },
+      nextEl: ".custom-btn-next",
+      prevEl: ".custom-btn-prev",
+    },
   });
+
+
+  // offcanvas bar 
+  $(".td-offcanvas-toggle").on('click', function () {
+    $(".td-offcanvas").addClass("td-offcanvas-open");
+    $(".td-offcanvas-overlay").addClass("td-offcanvas-overlay-open");
+  });
+  $(".td-offcanvas-close-toggle,.td-offcanvas-overlay").on('click', function () {
+    $(".td-offcanvas").removeClass("td-offcanvas-open");
+    $(".td-offcanvas-overlay").removeClass("td-offcanvas-overlay-open");
+  });
+
+
+})(jQuery);

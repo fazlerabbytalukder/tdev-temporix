df2601ee26f0cbbd56bea4bc85fa76a289e00e0b
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-11-22 17:31:43 +0600

    brand section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 (function ($) {
   "use strict";
 
+
+  // hero image slider 
   var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
@@ -20,6 +22,45 @@
   $(".td-offcanvas-close-toggle,.td-offcanvas-overlay").on('click', function () {
     $(".td-offcanvas").removeClass("td-offcanvas-open");
     $(".td-offcanvas-overlay").removeClass("td-offcanvas-overlay-open");
+  });
+
+
+  // brands slider
+  var swiper = new Swiper(".td-brand-active", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true, // Enables infinite loop
+    speed: 4000,
+    autoplay: {
+      delay: 1, // Delay between transitions (in milliseconds)
+      disableOnInteraction: false, // Keeps autoplay running after user interactions
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1400: {
+        slidesPerView: 5,
+        spaceBetween: 60,
+      },
+      1600: {
+        slidesPerView: 6,
+        spaceBetween: 60,
+      },
+    },
   });
 
 

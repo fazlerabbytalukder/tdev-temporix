d673311e5aef1b94ddacfaa3be0d3ba0297b756d
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-11-22 11:36:15 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".custom-btn-next",
+        prevEl: ".custom-btn-prev",
+      },
+  });
